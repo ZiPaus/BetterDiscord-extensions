c508d45fa01fa532915b65137a51b1c3c0b07cd9
Antti <antti-kettunen@hotmail.com>
2017-01-09 22:31:45 +0200

    ToggleSections v1.4
    - Fix misc. crashes
    - Switch (from localStorage) to new BD settings storage
    - Refactor to ES6 class syntax

--- plugins/ToggleSections/ToggleSections.plugin.js
+++ plugins/ToggleSections/ToggleSections.plugin.js
@@ -9,218 +9,228 @@
 
     Changelog
     ---------
-    v1.0 (April 3rd 2016): Initial version
-    v1.1 (April 5th 2016): Added initial settings (choose which buttons to show, modify button color)
-    v1.2 (April 6th 2016): Improved settings, bug fixes
-    v1.3 (April 7th 2016): Refactoring
+    v1.0    (April 3rd 2016):       Initial version
+    v1.1    (April 5th 2016):       Added initial settings (choose which buttons to show, modify button color)
+    v1.2    (April 6th 2016):       Improved settings, bug fixes
+    v1.3    (April 7th 2016):       Refactoring
+    v1.4    (January 10th 2017):    Fix misc. crashes, switch to new BD settings storage, refactor to ES6 class syntax
  */
 
-ToggleSections = (function() {
+class ToggleSections {
 
-var tssettings = {};
-var containers = [
-    { label: 'Server list', className: 'guilds-wrapper', position: 'right' },
-    { label: 'Channel list', className: 'channels-wrap', position: 'right' }
-];
+    constructor() {
+        // Default settings for the first run, after that stored settings will be used
+        this.settings = {
+            enabled: [true, true],
+            closed: [false, false],
+            color: "#738BD7",
+        };
+        this.containers = [
+            { label: 'Server list', className: 'guilds-wrapper', position: 'right' },
+            { label: 'Channel list', className: 'channels-wrap', position: 'right' },
+        ];
 
-function ToggleSections() {}
-
-ToggleSections.prototype.getName = function() {
-    return "Toggle Sections";
-};
-ToggleSections.prototype.getDescription = function() {
-    return "Allows you to hide sections of the program (check settings to modify)";
-};
-ToggleSections.prototype.getVersion = function() {
-    return "1.3";
-};
-ToggleSections.prototype.getAuthor = function() {
-    return "kettui /Cin";
-};
-ToggleSections.prototype.load = function() {};
-ToggleSections.prototype.unload = function() {};
-ToggleSections.prototype.onMessage = function() {};
-
-ToggleSections.prototype.start = function() {
-    if(!localStorage.getItem("tssettings")) localStorage.setItem("tssettings", JSON.stringify({
-        enabled: [true, true, true],
-        closed: [false, false, false],
-        color: "#3C3C3C"
-    }));
-
-    tssettings = JSON.parse(localStorage.getItem("tssettings"));
-
-    this.onSwitch();
-    this.addStyling();
-};
-
-ToggleSections.prototype.onSwitch = function() {
-    var self = this;
-
-    containers.forEach(function(container, i) {
-        self.attachHandler(container, i);
-    });
-};
-
-ToggleSections.prototype.observer = function (e) {
-    if(e.target.classList.contains('toggleable')) this.onSwitch();
-};
-
-ToggleSections.prototype.stop = function() {
-    $("#toggle-sections").remove();
-    for (var i = 0; i < containers.length; i++) {
-        $('#toggle-'+ containers[i].className).off("click.ts");
-        $('#toggle-'+ containers[i].className).remove();
-    }
-};
-
-ToggleSections.prototype.attachHandler = function(container, i) {
-    var section = $("."+container.className),
-        self = this,
-        buttonExists = $("#toggle-"+ container.className).length > 0;
-    if(buttonExists && !tssettings.enabled[i]) {
-        $("#toggle-"+ container.className).off("click.ts");
-        $("#toggle-"+ container.className).remove();
-        return;
+        this.attachHandler = this.attachHandler.bind(this);
+        this.start = this.start.bind(this);
+        this.onSwitch = this.onSwitch.bind(this);
+        this.observer = this.observer.bind(this);
+        this.stop = this.stop.bind(this);
+        this.attachHandler = this.attachHandler.bind(this);
+        this.addStyling = this.addStyling.bind(this);
+        this.getSettingsPanel = this.getSettingsPanel.bind(this);
+        this.updateSettings = this.updateSettings.bind(this);
     }
 
-    if(buttonExists || !tssettings.enabled[i]) return;
+    getName() {
+        return "Toggle Sections";
+    }
 
-    section.append('<div class="toggle-section '+ container.position +'" id="toggle-'+ container.className +'"></div>');
-    section.addClass("toggleable");
+    getDescription() {
+        return "Allows you to hide sections of the program (check settings to modify)";
+    }
 
-    var btn = $('#toggle-'+ container.className +'');
+    getVersion() {
+        return "1.4";
+    }
 
-    var toggleSection = function() {
-        tssettings.closed[i] ? section.addClass("closed") : section.removeClass("closed");
+    getAuthor() {
+        return "kettui /Cin";
+    }
+
+    load() {}
+    unload() {}
+    onMessage() {}
+
+    start() {
+        const { settings, onSwitch, addStyling } = this;
+
+        if(!bdPluginStorage.get("ToggleSections", "settings"))
+            bdPluginStorage.set("ToggleSections", "settings", JSON.stringify(settings));
+
+        this.settings = JSON.parse(bdPluginStorage.get("ToggleSections", "settings"));
+
+        onSwitch();
+        addStyling();
+    }
+
+    onSwitch() {
+        const { containers, attachHandler } = this;
+        containers.forEach(attachHandler);
+    }
+
+    observer(e) {
+        if(e.target.classList.contains('toggleable'))
+            this.onSwitch();
+    }
+
+    stop() {
+        $("#toggle-sections").remove();
+        this.containers.forEach(container => {
+            $('#toggle-'+ container.className).off("click.ts");
+            $('#toggle-'+ container.className).remove();
+        });
     };
 
-    var handleClick = function() {
-        var isClosed = tssettings.closed[i] ? false : true;
-        tssettings.closed[i] = isClosed;
-        self.updateSettings();
-        toggleSection();
-    };
+    attachHandler(container, i) {
+        const { settings, updateSettings } = this;
 
-    if(tssettings.closed[i]) toggleSection();
+        const section = $("."+container.className);
+        const buttonExists = $("#toggle-"+ container.className).length > 0;
 
-    $('.toggle-section').css({
-        'border-color': 'transparent '+ tssettings.color
-    });
+        if(buttonExists && !settings.enabled[i]) {
+            $("#toggle-"+ container.className).off("click.ts");
+            $("#toggle-"+ container.className).remove();
+            return;
+        }
 
-    btn.on("click.ts", handleClick);
-}
+        if(buttonExists || !settings.enabled[i]) return;
 
+        section.append('<div class="toggle-section '+ container.position +'" id="toggle-'+ container.className +'"></div>');
+        section.addClass("toggleable");
 
-ToggleSections.prototype.addStyling = function() {
-    if($("#toggle-sections").length) $("#toggle-sections").html("");
+        const btn = $('#toggle-'+ container.className +'');
 
-    var css = [
-        '.channel-members-wrap { min-width: 0; }',
+        const toggleSection = () => {
+            settings.closed[i] ? section.addClass("closed") : section.removeClass("closed");
+        }
 
-        '.toggleable.closed { overflow: visible !important; width: 0 !important; }',
-        '.toggleable.closed > *:not(.toggle-section) { opacity: 0 !important; }',
+        const handleClick = () => {
+            const isClosed = this.settings.closed[i] ? false : true;
+            settings.closed[i] = isClosed;
+            updateSettings();
+            toggleSection();
+        };
 
-    	'.toggle-section {',
-    		'position: absolute;',
-    		'bottom: 0;',
-            'z-index: 6;',
-            'cursor: pointer;',
-    		'opacity: .4 !important;',
-            'border-width: 10px 0;',
-    		'border-style: solid;',
-            'border-color: transparent '+tssettings.color,
-    	'}',
+        if(settings.closed[i])
+            toggleSection();
 
-        '.toggle-section:hover { opacity: 1 !important; }',
-
-    	'.toggle-section.right { right: 0; border-right-width: 10px; }',
-
-    	'.toggleable.closed .toggle-section.right {',
-    		'right: -10px;',
-    		'border-left-width: 10px;',
-    		'border-right-width: 0;',
-    	'}',
-
-    	'.toggle-section.left { left: 0; border-left-width: 10px }',
-
-    	'.toggleable.closed .toggle-section.left {',
-    		'left: -10px;',
-    		'border-right-width: 10px;',
-    		'border-left-width: 0;',
-    	'}',
-
-    	'.channel-members-wrap.closed .toggle-section { left: -30px; }'
-    ];
-
-    // Ensure that the containers are positioned relatively
-    for (var i = 0; i < containers.length; i++) {
-        css.push('.'+containers[i].className+'{ position: relative; transition: width 150ms; }');
+        btn.on("click.ts", handleClick);
     }
 
-    css = css.join(' ');
+    addStyling() {
+        const { containers, settings } = this;
+        if($("#toggle-sections").length) $("#toggle-sections").html("");
 
-    if ($("#toggle-sections").length == 0) {
-        $("head").append('<style id="toggle-sections"></style>');
-    }
+        let css = [
+            '.channel-members-wrap { min-width: 0; }',
 
-    $("#toggle-sections").append(css);
-}
+            '.toggleable.closed { overflow: visible !important; width: 0 !important; }',
+            '.toggleable.closed > *:not(.toggle-section) { opacity: 0 !important; }',
 
-ToggleSections.prototype.getSettingsPanel = function () {
-    var self = this;
-    var settingsContainer = $('<div/>', { id: "ts-settings" });
-    var colorPicker = $("<input/>", {
-        type: "color",
-        class: "swatch default",
-        id: "color-picker"
-    });
-    colorPicker.prop("value", tssettings.color);
+            '.toggle-section {',
+                'position: absolute;',
+                'bottom: 0;',
+                'z-index: 6;',
+                'cursor: pointer;',
+                'opacity: .4 !important;',
+                'border-width: 10px 0;',
+                'border-style: solid;',
+                'border-color: transparent '+settings.color,
+            '}',
 
-    for (var i = 0; i < containers.length; i++) {
-        var container = containers[i];
+            '.toggle-section:hover { opacity: 1 !important; }',
 
-        var checkbox = $('<input />', {
-            type: 'checkbox',
-            'data-i': i,
-            id: 'ts-'+ container.className,
-            checked: tssettings.enabled[i] ? true : false,
-            click: function() {
-                var elem = $(this),
-                    isChecked = elem.attr("checked"),
-                    u = parseInt(elem.attr('data-i'));
+            '.toggle-section.right { right: 0; border-right-width: 10px; }',
 
-                isChecked ? elem.prop("checked", false) : elem.prop("checked", true);
-                tssettings.enabled[u] = tssettings.enabled[u] ? false : true;
+            '.toggleable.closed .toggle-section.right {',
+                'right: -10px;',
+                'border-left-width: 10px;',
+                'border-right-width: 0;',
+            '}',
 
-                ToggleSections.prototype.updateSettings();
-                ToggleSections.prototype.onSwitch();
-            }
+            '.toggle-section.left { left: 0; border-left-width: 10px }',
+
+            '.toggleable.closed .toggle-section.left {',
+                'left: -10px;',
+                'border-right-width: 10px;',
+                'border-left-width: 0;',
+            '}',
+
+            '.channel-members-wrap.closed .toggle-section { left: -30px; }'
+        ];
+
+        // Ensure that the containers are positioned relatively
+        containers.forEach(container => {
+            css.push('.'+container.className+'{ position: relative; transition: width 150ms; }');
         });
 
-        var checkboxLabel = $('<span />', {
-            text: container.label
-        });
+        css = css.join(' ');
 
-        settingsContainer.append(checkbox, checkboxLabel, '<br/>');
+        if ($("#toggle-sections").length > 0)
+            $("#toggle-sections").remove();
+
+        $("head").append(`<style id="toggle-sections">${css}</style>`);
     }
 
-    settingsContainer.append('<span>Button color:</span>', colorPicker);
+    getSettingsPanel() {
+        const { containers, settings, addStyling, updateSettings, onSwitch } = this;
 
-    colorPicker.on("change", function() {
-        var newColor = $(this).prop("value");
-        tssettings.color = newColor;
-        self.updateSettings();
-        self.addStyling();
-    })
+        const settingsContainer = $('<div/>', { id: "ts-settings" });
+        const colorPicker = $("<input/>", {
+            type: "color",
+            class: "swatch default",
+            id: "color-picker"
+        });
+        colorPicker.prop("value", settings.color);
 
-    return settingsContainer;
-};
+        containers.forEach((container, i) => {
+            const checkbox = $('<input />', {
+                "type": 'checkbox',
+                "data-ts-i": i,
+                "id": 'ts-'+ container.className,
+                "checked": settings.enabled[i],
+                click() {
+                    const elem = $(this);
+                    const isChecked = elem.attr("checked");
+                    const u = parseInt(elem.attr("data-ts-i"));
 
-ToggleSections.prototype.updateSettings = function() {
-    localStorage.setItem("tssettings", JSON.stringify(tssettings));
-};
+                    settings.enabled[u] = settings.enabled[u] ? false : true;
 
-return ToggleSections;
+                    updateSettings();
+                    onSwitch();
+                }
+            });
 
-})();
+            const checkboxLabel = $('<span />', {
+                text: container.label
+            });
+
+            settingsContainer.append(checkbox, checkboxLabel, '<br/>');
+        });
+
+        settingsContainer.append('<span>Button color:</span>', colorPicker);
+
+        colorPicker.on("change", function() {
+            const newColor = $(this).prop("value");
+            settings.color = newColor;
+            updateSettings();
+            addStyling();
+        });
+
+        return settingsContainer;
+    }
+
+    updateSettings() {
+        bdPluginStorage.set("ToggleSections", "settings", JSON.stringify(this.settings));
+    }
+}
